43045ce4ff154128c87cc7ef3b66666e67c29730
Patrick Sturm <sturm@tofwerk.com>
2016-08-29 16:06:13 +0200

    Cleaned up zzz.R and TofDaqR.R.

--- R/TofDaqR.R
+++ R/TofDaqR.R
@@ -1,19 +1,15 @@
-#' TofDaqR: R wrapper of TOFWERK's TofDaq API functions.
+#' TofDaqR: R Interface to TOFWERK TofDaq API.
 #'
 #' The TofDaqR package is a wrapper for the TofDaq API functions provided by
 #' TofDaqDll.dll, TwH5Dll.dll and TwToolDll.dll.
 #'
-#' @section TofDaqR functions: TofDaqR functions can be used to configure the
-#'   TofDaq recorder application, control running acquisitions, access the data
-#'   being recorded and store arbitrary data synchronously or asynchronously to
-#'   the recorded mass spectra. \code{TPS...} functions allow to control the TOF Power
-#'   Supply. Further, there are functions to access and modify recorded data
-#'   stored in Tofwerk HDF5 files and additional "tool" functions, e.g. for peak
-#'   fitting.
+#' @section TofDaqR functions: TofDaqR functions can be used to configure the TofDaq recorder
+#' application, control running acquisitions, access the data in realtime and
+#' store arbitrary data synchronously or asynchronously to the recorded mass
+#' spectra. Further, there are functions allowing to control the TOF Power
+#' Supply and to access and modify recorded data stored in HDF5 files. The
+#' package also contains additional "tool" functions, e.g. for peak fitting.
 #'
 #' @docType package
 #' @name TofDaqR
-#'
-#' @useDynLib TofDaqR
-#' @importFrom Rcpp sourceCpp
 NULL
--- R/zzz.R
+++ R/zzz.R
@@ -1,17 +1,8 @@
-# .onLoad <-function(lib,pkg){
-#   ver <- read.dcf(file.path(lib, pkg, "DESCRIPTION"),
-#                   "Version")
-#   ver <- as.character(ver)
-#   library.dynam("TofDaqWrapR", pkg, lib)
-#   cat("mypackage", ver,file.path(lib,pkg), "loaded\n")
-# }
-
 .onLoad <- function(lib, pkg) {
-  library.dynam('TofDaqWrapR', pkg, lib)
+  ver <- as.character(read.dcf(file.path(lib, pkg, "DESCRIPTION"), "Version"))
+  cat("TofDaqR", ver, "loaded.\n")
 }
 
 .onUnload <- function(libpath) {
   CleanupDll()
-  # library.dynam.unload('TofDaqWrapR', libpath)  # this often crashes R
-  library.dynam.unload('TofDaqR', libpath)
 }
