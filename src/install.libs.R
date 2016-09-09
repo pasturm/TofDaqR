8793ddefa3d1c3e120e63bff71332cad6ffc5143
Patrick Sturm <sturm@tofwerk.com>
2016-09-09 09:40:55 +0200

    Removed obsolete TofDaqR.dll code.

--- src/install.libs.R
+++ src/install.libs.R
@@ -3,5 +3,3 @@ files <- Sys.glob(c(file.path(R_PACKAGE_SOURCE, paste0("src", R_ARCH), "*.dll"),
 dest <- file.path(R_PACKAGE_DIR, paste0("libs", R_ARCH))
 dir.create(dest, recursive = TRUE, showWarnings = FALSE)
 file.copy(files, dest, overwrite = TRUE)
-if(file.exists("TofDaqR.dll"))
-  file.copy("TofDaqR.dll", dest, overwrite = TRUE)
