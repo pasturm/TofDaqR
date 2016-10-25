2737bdc5425b8bfe516a49a681c70a7189d6b0b7
Patrick Sturm <sturm@tofwerk.com>
2016-10-25 17:33:26 +0200

    Updated package description.

--- R/TofDaqR.R
+++ R/TofDaqR.R
@@ -1,12 +1,13 @@
 #' TofDaqR: R Interface to TOFWERK TofDaq API.
 #'
-#' The TofDaqR package is a wrapper for the TofDaq API functions provided by
-#' TofDaqDll.dll, TwH5Dll.dll and TwToolDll.dll.
+#' The TofDaqR package is a wrapper for the TofDaq API functions provided by the
+#' shared libraries TofDaqDll, TwH5Dll and TwToolDll (Windows) and libtwh5 and
+#' libtwtool (macOS and Linux).
 #'
 #' @section TofDaqR functions: TofDaqR functions can be used to configure the TofDaq recorder
 #' application, control running acquisitions, access the data in realtime and
 #' store arbitrary data synchronously or asynchronously to the recorded mass
-#' spectra. Further, there are functions allowing to control the TOF Power
+#' spectra. Further, there are functions for controling the TOF Power
 #' Supply and to access and modify recorded data stored in HDF5 files. The
 #' package also contains additional "tool" functions, e.g. for peak fitting.
 #'
