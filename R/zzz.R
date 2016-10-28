38d3ebcbac10daf3b363505cb76b6ac62b90501e
Patrick Sturm <sturm@tofwerk.com>
2016-10-28 12:00:00 +0200

    Updated zzz.R.

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onLoad <- function(lib, pkg) {
   ver <- as.character(read.dcf(file.path(lib, pkg, "DESCRIPTION"), "Version"))
-  cat("TofDaqR", ver, "loaded.\n")
+  packageStartupMessage("TofDaqR ", ver, " loaded.")
 }
 
 .onUnload <- function(libpath) {
