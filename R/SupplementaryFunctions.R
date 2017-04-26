e2e342ae8a0b5dd9a85310f887c18f4402a6c3f4
Patrick Sturm <sturm@tofwerk.com>
2017-04-26 12:14:50 +0200

    Added wrapper functions GetTofDataSinglePeak() and FitTofDataSinglePeak().

--- R/SupplementaryFunctions.R
+++ R/SupplementaryFunctions.R
@@ -113,3 +113,148 @@ SaveMassTableToFile = function(filename = "TmpMassTable.txt") {
                 quote = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE)
   }
 }
+
+# GetTofDataSinglePeak ---------------------------------------------------------
+#' Gets TofData of a single peak from a HDF5 data file.
+#'
+#' \code{GetTofDataSinglePeak} gets TofData of a single peak or a specified mass
+#' range from a HDF5 data file.
+#'
+#' Note: this function is not part of the TofDaq API, but is included in the
+#' package for convenience.
+#'
+#' @param filename Path/filename.
+#' @param peakIndex Peak index (one-based).
+#' @param massRange Mass range in Th. Specified as a two-element vector
+#' containing the low and high limit of the mass range. If \code{NULL} the mass
+#' range is taken from the peak parameter list.
+#' @param secondTOF If \code{TRUE} the data are read from /FullSpectra2/TofData
+#' @return A list containing the mass axis, time axis, TofData and averaged
+#' spectrum of the peak, and the descriptor of the data file.
+#'
+#' @family wrapper functions
+#'
+#' @export
+GetTofDataSinglePeak = function(filename, peakIndex, massRange = NULL,
+                                secondTOF = FALSE) {
+
+  # read mass axis data
+  if (secondTOF) {
+    MassAxis = GetSpecXaxisFromH5(filename, Type = -1, writeIndex = 0)
+    TimeAxis = GetSpecXaxisFromH5(filename, Type = -2, writeIndex = 0)
+  } else {
+    MassAxis = GetSpecXaxisFromH5(filename, Type = 1, writeIndex = 0)
+    TimeAxis = GetSpecXaxisFromH5(filename, Type = 2, writeIndex = 0)
+  }
+
+  if (is.null(massRange)) {
+    peakpar = GetPeakParametersFromH5(filename, PeakIndex = peakIndex - 1)
+  } else {
+    peakpar = data.frame(loMass = massRange[1], hiMass = massRange[2])
+  }
+
+  idx = which(MassAxis>(peakpar$loMass)&MassAxis<(peakpar$hiMass))  # sample indices
+
+  # Read H5descriptor
+  desc = GetH5Descriptor(filename)
+
+  # read FullSpectra data of one mass
+  if (secondTOF) {
+    TofData = GetTofData2(filename,  idx[1]-1, length(idx), 0, desc$nbrSegments, 0, desc$nbrBufs, 0, desc$nbrWrites)
+  } else {
+    TofData = GetTofData(filename,  idx[1]-1, length(idx), 0, desc$nbrSegments, 0, desc$nbrBufs, 0, desc$nbrWrites)
+  }
+  # convert units to mV
+  TofData = TofData/desc$nbrWaveforms
+
+  # average over buf and write dimension
+  TofData = matrix(TofData, ncol = desc$nbrBufs*desc$nbrWrites)
+  AverageSpectrum = rowSums(TofData)/(desc$nbrBufs*desc$nbrWrites)
+
+  return(list(MassAxis = MassAxis[idx], TimeAxis = TimeAxis[idx],
+              TofData = TofData, AverageSpectrum = AverageSpectrum,
+              desc = desc))
+}
+
+# FitTofDataSinglePeak ---------------------------------------------------------
+#' Wrapper function for FitSinglePeak().
+#'
+#' \code{FitTofDataSinglePeak} is a wrapper for \code{\link{FitSinglePeak}}
+#' and also reports fitted resolving power, area and count rates. It takes the
+#' output of \code{\link{GetTofDataSinglePeak}} as input parameter.
+#'
+#' Peak types:
+#' \tabular{cccc}{
+#' peak type index \tab peak function \tab symmetric \tab baseline \cr
+#' -1 \tab Gaussian through highest point and its neighbours \tab yes \tab no \cr
+#' 0 \tab Gaussian \tab yes \tab no  \cr
+#' 1 \tab Lorentzian \tab yes \tab no \cr
+#' 2 \tab Pseudo-Voigt \tab yes \tab no \cr
+#' 3 \tab Gaussian \tab yes \tab yes \cr
+#' 4 \tab Lorentzian \tab yes \tab yes \cr
+#' 5 \tab Pseudo-Voigt \tab yes \tab yes \cr
+#' 6 \tab Gaussian \tab no \tab no \cr
+#' 7 \tab Lorentzian \tab no \tab no \cr
+#' 8 \tab Pseudo-Voigt \tab no \tab no \cr
+#' 9 \tab Gaussian \tab no \tab yes \cr
+#' 10 \tab Lorentzian \tab no \tab yes \cr
+#' 11 \tab Pseudo-Voigt \tab no \tab yes
+#' }
+#'
+#' Note: this function is not part of the TofDaq API, but is included in the
+#' package for convenience.
+#'
+#' @param PeakTofData List from the output of \code{\link{GetTofDataSinglePeak}}.
+#' @param peakType Peak model to use.
+#' @return A list with fitted parameters.
+#'
+#' @family wrapper functions
+#'
+#' @export
+FitTofDataSinglePeak = function(PeakTofData, peakType) {
+
+  # initial values
+  mu = 0.5 # mixing parameter
+  amplitude = max(PeakTofData$AverageSpectrum) # amplitude
+
+  # TimeAxis
+  peakPos = PeakTofData$TimeAxis[PeakTofData$AverageSpectrum==amplitude] # position in tof (ns)
+  fwhm = diff(range(PeakTofData$TimeAxis[PeakTofData$AverageSpectrum>amplitude/2], na.rm=TRUE)) # FWHM in tof (ns)
+  fwhm = max(fwhm, min(diff(PeakTofData$TimeAxis))) # make it > 0
+
+  fit1 = FitSinglePeak(yVals = PeakTofData$AverageSpectrum, xVals = PeakTofData$TimeAxis, peakType = peakType,
+                       amplitude = amplitude, fwhmLo = fwhm, fwhmHi = fwhm,
+                       peakPos = peakPos, mu = mu)
+
+  # resolution
+  fit1$resolution = fit1$peakPos/(fit1$fwhmLo+fit1$fwhmHi)
+
+  # integrate mV over time axis (microsec) to get mV*ns
+  if (any(peakType == c(0, 3, 6, 9))) {
+    fit1$area = fit1$amplitude*(fit1$fwhmLo+fit1$fwhmHi)/2*sqrt(pi/log(2))/2*1000  # Gaussian
+  } else if (any(peakType == c(1, 4, 7, 10))) {
+    fit1$area = fit1$amplitude*(fit1$fwhmLo+fit1$fwhmHi)/2*pi/2*1000  # Lorentzian
+  } else if (any(peakType == c(2, 5, 8, 11))) {
+    fit1$area = fit1$amplitude*(fit1$fwhmLo+fit1$fwhmHi)/2/2*(fit1$mu*pi+(1-fit1$mu)*sqrt(pi/log(2)))*1000  # Pseudo-Voigt
+  }
+
+  # normalize with single ion signal to get counts per extraction
+  fit1$cpe = fit1$area/PeakTofData$desc$singleIonSignal
+  # counts per second
+  fit1$cps = fit1$cpe/PeakTofData$desc$tofPeriod
+
+  # MassAxis
+  peakPos = PeakTofData$MassAxis[PeakTofData$AverageSpectrum==amplitude] # position in m/Q (Th)
+  fwhm = diff(range(PeakTofData$MassAxis[PeakTofData$AverageSpectrum>amplitude/2], na.rm=TRUE)) # FWHM in m/Q (Th)
+  fwhm = max(fwhm, min(diff(PeakTofData$MassAxis))) # make it > 0
+
+  fit2 = FitSinglePeak(yVals = PeakTofData$AverageSpectrum, xVals = PeakTofData$MassAxis, peakType = peakType,
+                       amplitude = amplitude, fwhmLo = fwhm, fwhmHi = fwhm,
+                       peakPos = peakPos, mu = mu)
+
+  # resolution
+  fit2$resolution = fit2$peakPos/(fit2$fwhmLo+fit2$fwhmHi)*2
+
+  return(list(fit_time = fit1, fit_mass = fit2))
+}
+
